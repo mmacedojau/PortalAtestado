768665cb9890230459c8a8be8dbcd9df30de3372
Matheus Macedo <matheusmacedojau@hotmail.com>
2023-03-01 20:40:48 -0300

    Criado a Parte de Lista de Atestados, o READ do CRUD

--- listar-atestado.php
+++ listar-atestado.php
@@ -1,1 +1,29 @@
 <h1> Listar Atestados </h1>
+<?php 
+    $sql = "SELECT * FROM eventos";
+    
+    $res = $conn->query($sql);
+
+    $qtd = $res->num_rows;
+
+    if($qtd > 0){
+            print"<table class='table table-hover table-striped table-bordered'>";
+            print "<tr>";
+            print "<th>#</th>";
+            print "<th>Nome</th>";
+            print "<th>Setor</th>";
+            print "<th>Data</th>";
+            print "</tr>";
+        while($row = $res->fetch_object()){
+            print "<tr>";
+            print "<td>".$row->id."</td>";
+            print "<td>".$row->nome."</td>";
+            print "<td>".$row->setor."</td>";
+            print "<td>".$row->data."</td>";
+            print "</tr>";
+        }
+        print "</table>";
+    }else{
+        print "<p class='alert alert-danger'>Não encontrou resultados!</p>";
+    }
+?>
