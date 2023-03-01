e5a4bea2ce9cea83a4b3cd76691d45b306062382
Matheus Macedo <matheusmacedojau@hotmail.com>
2023-02-28 22:36:14 -0300

    Atualização no Título

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portal de Atestados 2023</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" >
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Portal de Atestados</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Início</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="novo-atestado.php">Novo Atestado</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="listar-atestado.php">Listar Atestados</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+    <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
