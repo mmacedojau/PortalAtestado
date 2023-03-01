e95acf1d49f515936febe2ae8b59f8d712b5882d
Matheus Macedo <matheusmacedojau@hotmail.com>
2023-03-01 20:24:44 -0300

    Finalizado Crud. Adicionado mensagem em javascript

--- salvar-atestado.php
+++ salvar-atestado.php
@@ -9,6 +9,14 @@
             $sql = "INSERT INTO eventos (nome, matricula, setor, data) VALUES ('{$nome}','{$matricula}','{$setor}','{$data}')";
 
             $res = $conn->query($sql);
+            
+            if($res==true){
+                print "<script>alert('Atestado Cadastrado com Sucesso !');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }else{
+                print "<script>alert('Não foi possível cadastrar o atestado');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }
             break;
 
         case 'editar':
