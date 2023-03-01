ff87e2586d1a5ecc7a5e7f082f066d92b7daf41e
Matheus Macedo <matheusmacedojau@hotmail.com>
2023-03-01 19:34:50 -0300

    Criado conexão com Banco de Dados

--- index.php
+++ index.php
@@ -7,26 +7,48 @@
     <link href="css/bootstrap.min.css" rel="stylesheet" >
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Portal de Atestados</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Início</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="novo-atestado.php">Novo Atestado</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="listar-atestado.php">Listar Atestados</a>
-        </li>
-      </ul>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Portal de Atestados</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php">Início</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?page=novo">Novo Atestado</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?page=listar">Listar Atestados</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="row">
+        <div class="col mt-5">
+          <?php
+            include("config.php");
+            switch(@$_REQUEST["page"]){
+              case "novo":
+                include("novo-atestado.php");
+                break;
+              case "listar":
+                include("listar-atestado.php");
+                break;
+              default:
+                print "<h1>Bem Vindo</h1>";
+            }
+          ?>
+        </div>
+      </div>
     </div>
-  </div>
+
     <script src="js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
